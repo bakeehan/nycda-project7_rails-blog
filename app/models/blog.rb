e3b7a2e65ad4d3b3e48934416e79f781a5dea4c4
Bridget Keehan <ba.keehan@gmail.com>
2018-07-04 12:47:16 -0400

    models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+	has_many :comments
+	belongs_to :user
+	serialize :taggable, Array
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :blogs
+  resources :comments
   devise_for :users
 	root "blogs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
